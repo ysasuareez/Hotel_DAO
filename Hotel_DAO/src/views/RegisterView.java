a49aa4d74957dcf52e8614baad5daf342c11b2da
RafaSepSal <93984765+RafaSepSal@users.noreply.github.com>
2022-02-15 12:45:07 +0100

    Add files via upload

---
+++ Hotel_DAO/src/views/RegisterView.java
@@ -0,0 +1,155 @@
+package views;
+
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JOptionPane;
+import javax.swing.SwingConstants;
+
+import models.Usuario;
+
+import java.awt.Font;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+import javax.swing.JTextField;
+import javax.swing.JPasswordField;
+import javax.swing.JButton;
+import java.awt.Color;
+
+public class RegisterView {
+
+	private JFrame frame;
+	private JTextField tfDNI;
+	private JTextField tfNombre;
+	private JTextField tfEdad;
+	private JTextField tfUser;
+	private JPasswordField passwordRegister;
+	private JLabel lblDNI;
+	private JLabel lblApellido;
+	private JLabel lblRegistro;
+	private JLabel lblNewLabel_4;
+	private JLabel lblEdad;
+	private JLabel lblUser;
+	private JLabel lblContraseña;
+	public JButton btnRegistrar;
+	
+
+	/**
+	 * Create the application.
+	 */
+	public RegisterView() {
+		initialize();
+	}
+
+	/**
+	 * Initialize the contents of the frame.
+	 */
+	private void initialize() {
+		frame = new JFrame();
+		frame.getContentPane().setBackground(new Color(135, 206, 250));
+		frame.setVisible(true);
+		frame.setBounds(100, 100, 450, 300);
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame.getContentPane().setLayout(null);
+		
+		lblRegistro = new JLabel("REGISTRO");
+		lblRegistro.setFont(new Font("SimSun-ExtB", Font.BOLD, 22));
+		lblRegistro.setHorizontalAlignment(SwingConstants.CENTER);
+		lblRegistro.setBounds(100, 0, 223, 34);
+		frame.getContentPane().add(lblRegistro);
+		
+		tfDNI = new JTextField();
+		tfDNI.setBounds(119, 70, 86, 20);
+		frame.getContentPane().add(tfDNI);
+		tfDNI.setColumns(10);
+		
+		tfNombre = new JTextField();
+		tfNombre.setBounds(119, 116, 86, 20);
+		frame.getContentPane().add(tfNombre);
+		tfNombre.setColumns(10);
+		
+		lblDNI = new JLabel("DNI");
+		lblDNI.setForeground(new Color(0, 0, 255));
+		lblDNI.setBackground(new Color(0, 0, 255));
+		lblDNI.setHorizontalAlignment(SwingConstants.RIGHT);
+		lblDNI.setBounds(40, 73, 69, 14);
+		frame.getContentPane().add(lblDNI);
+		
+		lblApellido = new JLabel("Nombre y");
+		lblApellido.setForeground(new Color(0, 0, 255));
+		lblApellido.setHorizontalAlignment(SwingConstants.RIGHT);
+		lblApellido.setBounds(12, 116, 97, 20);
+		frame.getContentPane().add(lblApellido);
+		
+		lblNewLabel_4 = new JLabel("apellidos");
+		lblNewLabel_4.setForeground(new Color(0, 0, 255));
+		lblNewLabel_4.setHorizontalAlignment(SwingConstants.RIGHT);
+		lblNewLabel_4.setBounds(12, 132, 97, 14);
+		frame.getContentPane().add(lblNewLabel_4);
+		
+		tfEdad = new JTextField();
+		tfEdad.setBounds(311, 70, 86, 20);
+		frame.getContentPane().add(tfEdad);
+		tfEdad.setColumns(10);
+		
+		tfUser = new JTextField();
+		tfUser.setBounds(311, 116, 86, 20);
+		frame.getContentPane().add(tfUser);
+		tfUser.setColumns(10);
+		
+		lblEdad = new JLabel("Edad");
+		lblEdad.setForeground(new Color(0, 0, 255));
+		lblEdad.setHorizontalAlignment(SwingConstants.RIGHT);
+		lblEdad.setBounds(215, 73, 86, 14);
+		frame.getContentPane().add(lblEdad);
+		
+		lblUser = new JLabel("Usuario");
+		lblUser.setForeground(new Color(0, 0, 255));
+		lblUser.setHorizontalAlignment(SwingConstants.RIGHT);
+		lblUser.setBounds(208, 119, 93, 14);
+		frame.getContentPane().add(lblUser);
+		
+		passwordRegister = new JPasswordField();
+		passwordRegister.setBounds(119, 165, 86, 20);
+		frame.getContentPane().add(passwordRegister);
+		
+		lblContraseña = new JLabel("Contrase\u00F1a");
+		lblContraseña.setForeground(new Color(0, 0, 255));
+		lblContraseña.setHorizontalAlignment(SwingConstants.RIGHT);
+		lblContraseña.setBounds(16, 168, 93, 14);
+		frame.getContentPane().add(lblContraseña);
+		
+		btnRegistrar = new JButton("Registrar");
+		btnRegistrar.setBackground(new Color(0, 0, 139));
+		btnRegistrar.setForeground(new Color(224, 255, 255));
+		btnRegistrar.setFont(new Font("DejaVu Serif", Font.BOLD, 12));
+		btnRegistrar.addActionListener(new ActionListener() {
+
+			public void actionPerformed(ActionEvent e) {
+				String username = tfUser.getText();
+				String password = new String(passwordRegister.getPassword());
+				String nombre = tfNombre.getText();
+				int edad = Integer.parseInt(tfEdad.getText());
+				String dni = tfDNI.getText();
+				
+				// / / / / / / / / / CONTROL DE ERRORES / / / / / / / / 
+				if(edad < 18) {
+						JOptionPane.showMessageDialog(btnRegistrar, "Debes ser mayor de 18 años para registrarte");
+					} else {
+						
+						Usuario u1 = new Usuario(username, password, nombre, edad, dni);
+						Usuario.users.add(u1);
+						JOptionPane.showMessageDialog(btnRegistrar, "Registrado correctamente");
+						
+						new LoginView();
+						frame.dispose();
+					}
+			}
+				
+			
+			
+		});
+		btnRegistrar.setBounds(297, 165, 115, 34);
+		frame.getContentPane().add(btnRegistrar);
+	}
+}
---
+++ Hotel_DAO/src/views/ReservaView.java
@@ -0,0 +1,187 @@
+package views;
+
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JOptionPane;
+import javax.swing.SwingConstants;
+
+import models.Bungalow;
+import models.Bungalow.Estado;
+
+import java.awt.Font;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.util.ArrayList;
+import java.awt.Color;
+import javax.swing.JButton;
+import javax.swing.JTextField;
+
+@SuppressWarnings("unused")
+public class ReservaView {
+
+	private int indexVista;
+	private JFrame frame;
+	private JLabel lblTitulo;
+	private JLabel lblNombre;
+	private JLabel lblPersonas;
+	private JLabel lblHabitaciones;
+	private JLabel lblBaños;
+	private JLabel lblPrecio;
+	private JLabel lblDisponibilidad;
+	private JButton btnReservar;
+	private JButton btnAnterior;
+	private JButton btnSiguiente;
+	@SuppressWarnings("unused")
+	private Bungalow b = new Bungalow(null, null, indexVista, indexVista, indexVista, indexVista);
+
+	/**
+	 * Create the application.
+	 */
+	public ReservaView(int indexVista) {
+		initialize();
+		this.indexVista = indexVista;
+	}
+
+	/**
+	 * Initialize the contents of the frame.
+	 */
+	private void initialize() {
+		frame = new JFrame();
+		frame.getContentPane().setBackground(new Color(135, 206, 250));
+		frame.setVisible(true);
+		frame.setBounds(100, 100, 450, 300);
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame.getContentPane().setLayout(null);
+		
+	// / / / / / / / / / / / / ARRAYLIST DE BUNGALOWS Y ADICCIÓN DE ELLOS AL ARRAY / / / / / / / /
+		ArrayList<Bungalow> bungalows = new ArrayList<Bungalow>();
+		bungalows.add(new Bungalow(Estado.Disponible, "Zeus", 4, 5, 3, 200));
+		bungalows.add(new Bungalow(Estado.Disponible, "Poseidon", 4, 4, 3, 180));
+		bungalows.add(new Bungalow(Estado.Disponible, "Hades", 4, 3, 3, 165));
+		bungalows.add(new Bungalow(Estado.Disponible, "Ares", 4, 5, 4, 225));
+		bungalows.add(new Bungalow(Estado.Disponible, "Cronos", 4, 4, 2, 170));
+		bungalows.add(new Bungalow(Estado.Disponible, "Afrodita", 2, 1, 1, 100));
+		bungalows.add(new Bungalow(Estado.Disponible, "Apolo", 2, 1, 2, 110));
+		bungalows.add(new Bungalow(Estado.Disponible, "Baco", 2, 2, 3, 150));
+		bungalows.add(new Bungalow(Estado.Disponible, "Aquiles", 2, 2, 2, 135));
+		bungalows.add(new Bungalow(Estado.Disponible, "Cupido", 2, 1, 1, 120));
+		
+		// / / / / / / / / / DATOS A MOSTRAR DE LOS BUNGALOWS / / / / / / / /
+		Bungalow bungalowVista = bungalows.get(indexVista);
+		lblTitulo = new JLabel("RESERVAR");
+		lblTitulo.setFont(new Font("SimSun-ExtB", Font.BOLD, 22));
+		lblTitulo.setHorizontalAlignment(SwingConstants.CENTER);
+		lblTitulo.setBounds(120, 11, 170, 36);
+		frame.getContentPane().add(lblTitulo);
+		
+		lblNombre = new JLabel("Casa " + bungalowVista.getNombre());
+		lblNombre.setFont(new Font("Georgia", Font.BOLD | Font.ITALIC, 18));
+		lblNombre.setBounds(10, 53, 155, 24);
+		frame.getContentPane().add(lblNombre);
+		
+		lblPersonas = new JLabel(bungalowVista.getPersonas() + " personas");
+		lblPersonas.setFont(new Font("Georgia", Font.BOLD, 16));
+		lblPersonas.setBounds(10, 99, 131, 24);
+		frame.getContentPane().add(lblPersonas);
+		
+		lblHabitaciones = new JLabel(bungalowVista.getHabitaciones() + " habitaciones");
+		lblHabitaciones.setFont(new Font("Georgia", Font.PLAIN, 14));
+		lblHabitaciones.setBounds(10, 134, 131, 24);
+		frame.getContentPane().add(lblHabitaciones);
+		
+		lblBaños = new JLabel(bungalowVista.getBaños() + " baños");
+		lblBaños.setFont(new Font("Georgia", Font.PLAIN, 14));
+		lblBaños.setBounds(10, 169, 131, 24);
+		frame.getContentPane().add(lblBaños);
+		
+		lblPrecio = new JLabel(bungalowVista.getPrecio() + " €");
+		lblPrecio.setFont(new Font("Georgia", Font.PLAIN, 14));
+		lblPrecio.setBounds(10, 204, 131, 24);
+		frame.getContentPane().add(lblPrecio);
+		
+		lblDisponibilidad = new JLabel("DISPONIBLE");
+		lblDisponibilidad.setHorizontalAlignment(SwingConstants.CENTER);
+		lblDisponibilidad.setFont(new Font("Sylfaen", Font.BOLD, 25));
+		lblDisponibilidad.setForeground(Color.GREEN);
+		lblDisponibilidad.setBounds(203, 53, 221, 46);
+		frame.getContentPane().add(lblDisponibilidad);
+		
+		b = bungalows.get(indexVista);
+		
+		btnReservar = new JButton("Reservar");
+		btnReservar.setForeground(new Color(224, 255, 255));
+		btnReservar.setBackground(new Color(0, 0, 139));
+		btnReservar.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				Bungalow b = bungalows.get(indexVista);
+				if(b.getDisponible().equals(Estado.Disponible)) {
+					b.setDisponible(Estado.Ocupado);
+					JOptionPane.showMessageDialog(btnReservar, "¡Reserva realizada!");
+					checkReserva(b);
+				} else {
+					JOptionPane.showMessageDialog(btnReservar, "Reserva no disponible: bungalow ocupado");
+				}
+			}
+		});
+		
+		btnReservar.setFont(new Font("DejaVu Serif", Font.BOLD, 12));
+		btnReservar.setBounds(245, 193, 125, 46);
+		frame.getContentPane().add(btnReservar);
+		
+		btnAnterior = new JButton("Anterior");
+		btnAnterior.setFont(new Font("DejaVu Serif", Font.BOLD, 12));
+		btnAnterior.setForeground(new Color(224, 255, 255));
+		btnAnterior.setBackground(new Color(0, 0, 139));
+		btnAnterior.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				if(indexVista>0) {
+					indexVista --;
+					Bungalow b = bungalows.get(indexVista);
+					lblNombre.setText("Casa " + b.getNombre());
+					lblPersonas.setText(String.valueOf(b.getPersonas()) + " personas");
+					lblHabitaciones.setText(String.valueOf(b.getHabitaciones()) + " habitaciones");
+					lblBaños.setText(String.valueOf(b.getBaños()) + " baños");
+					lblPrecio.setText(String.valueOf(b.getPrecio()) + " €");
+					checkReserva(b);
+				}
+			}
+		});
+		
+		btnAnterior.setBounds(203, 110, 99, 23);
+		frame.getContentPane().add(btnAnterior);
+		
+		btnSiguiente = new JButton("Siguiente");
+		btnSiguiente.setFont(new Font("DejaVu Serif", Font.BOLD, 12));
+		btnSiguiente.setForeground(new Color(224, 255, 255));
+		btnSiguiente.setBackground(new Color(0, 0, 139));
+		btnSiguiente.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				if(indexVista<9) {
+					indexVista ++;
+					Bungalow b = bungalows.get(indexVista);
+					lblNombre.setText("Casa " + b.getNombre());
+					lblPersonas.setText(String.valueOf(b.getPersonas()) + " personas");
+					lblHabitaciones.setText(String.valueOf(b.getHabitaciones()) + " habitaciones");
+					lblBaños.setText(String.valueOf(b.getBaños()) + " baños");
+					lblPrecio.setText(String.valueOf(b.getPrecio()) + " €");
+					checkReserva(b);
+				}
+			}
+		});
+		btnSiguiente.setBounds(313, 110, 99, 23);
+		frame.getContentPane().add(btnSiguiente);
+		
+	}
+	
+	public void checkReserva(Bungalow b) {
+		
+		if(b.getDisponible().equals(Estado.Ocupado)) {
+			
+			lblDisponibilidad.setForeground(Color.RED);
+			lblDisponibilidad.setText("NO DISPONIBLE");
+		} else {
+			lblDisponibilidad.setForeground(Color.GREEN);
+			lblDisponibilidad.setText("DISPONIBLE");
+		}
+	}
+}
